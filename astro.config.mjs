3e12a42a2af58d452fb98077185238f4659ba872
unknown <your-github-email@example.com>
2026-05-30 22:29:04 +0700

    Remove Cloudflare adapter, use simple static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,9 +10,4 @@ export default defineConfig({
   base: process.env.ASTRO_BASE || '/',
 
   output: 'static',
-  adapter: cloudflare({
-    mode: 'static',
-    imageProcessing: { enabled: false },
-    serverless: { enabled: false },
-  }),
 });
